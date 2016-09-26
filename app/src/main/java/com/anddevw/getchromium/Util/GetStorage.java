1ef450df8743fe45215564c10d3a3b48ac648947
Andrew Wright <awrightdev@yahoo.com>
2016-09-26 14:51:16 -0700

    overhaul - fix broken installer

--- app/src/main/java/com/anddevw/getchromium/Util/GetStorage.java
+++ app/src/main/java/com/anddevw/getchromium/Util/GetStorage.java
@@ -15,50 +15,50 @@ public class GetStorage {
     private static final String ALTERNATE_SDCARD_MOUNTS[] =
             {"/emmc", "/sdcard/ext_sd", "/sdcard-ext", "/sdcard/sd", "/sdcard/sdcard"};
 
-public static File getDir(Context context, String dirName) {
-    File cacheDir = null;
+    public static File getDir(Context context, String dirName) {
+        File cacheDir = null;
 
-    if ( android.os.Environment.MEDIA_MOUNTED.equals(android.os.Environment.getExternalStorageState()) ) {
+        if ( android.os.Environment.MEDIA_MOUNTED.equals(android.os.Environment.getExternalStorageState()) ) {
 
-        Method getExternalFilesDirMethod = null;
-        try {
-            getExternalFilesDirMethod = Context.class.getMethod( "getExternalFilesDir", String.class );
-            cacheDir = (File) getExternalFilesDirMethod.invoke( context, dirName );
-        } catch (NoSuchMethodException e) {
-            cacheDir = buildCacheDirPath( context, android.os.Environment.getExternalStorageDirectory(), dirName );
-        } catch (IllegalArgumentException e) {
-            cacheDir = buildCacheDirPath( context, android.os.Environment.getExternalStorageDirectory(), dirName );
-        } catch (IllegalAccessException e) {
-            cacheDir = buildCacheDirPath( context, android.os.Environment.getExternalStorageDirectory(), dirName );
-        } catch (InvocationTargetException e) {
-            cacheDir = buildCacheDirPath( context, android.os.Environment.getExternalStorageDirectory(), dirName );
-        }
-    }
-
-    if ( cacheDir == null ) {
-
-        for ( int i = 0; i < ALTERNATE_SDCARD_MOUNTS.length; i++ ) {
-            File alternateDir = new File( ALTERNATE_SDCARD_MOUNTS[i] );
-            if ( alternateDir.exists() && alternateDir.isDirectory() &&
-                    alternateDir.canRead() && alternateDir.canWrite() ) {
-                cacheDir = buildCacheDirPath( context, alternateDir, dirName );
-                break;
+            Method getExternalFilesDirMethod = null;
+            try {
+                getExternalFilesDirMethod = Context.class.getMethod( "getExternalFilesDir", String.class );
+                cacheDir = (File) getExternalFilesDirMethod.invoke( context, dirName );
+            } catch (NoSuchMethodException e) {
+                cacheDir = buildCacheDirPath( context, android.os.Environment.getExternalStorageDirectory(), dirName );
+            } catch (IllegalArgumentException e) {
+                cacheDir = buildCacheDirPath( context, android.os.Environment.getExternalStorageDirectory(), dirName );
+            } catch (IllegalAccessException e) {
+                cacheDir = buildCacheDirPath( context, android.os.Environment.getExternalStorageDirectory(), dirName );
+            } catch (InvocationTargetException e) {
+                cacheDir = buildCacheDirPath( context, android.os.Environment.getExternalStorageDirectory(), dirName );
             }
         }
-    }
 
-    if ( cacheDir != null && !cacheDir.exists() ) {
-        if ( !cacheDir.mkdirs() ) {
-            cacheDir = null;
+        if ( cacheDir == null ) {
+
+            for ( int i = 0; i < ALTERNATE_SDCARD_MOUNTS.length; i++ ) {
+                File alternateDir = new File( ALTERNATE_SDCARD_MOUNTS[i] );
+                if ( alternateDir.exists() && alternateDir.isDirectory() &&
+                        alternateDir.canRead() && alternateDir.canWrite() ) {
+                    cacheDir = buildCacheDirPath( context, alternateDir, dirName );
+                    break;
+                }
+            }
         }
-    }
 
-    if ( cacheDir == null ) {
-        cacheDir = new File( context.getCacheDir() + File.separator + dirName );
-        cacheDir.mkdirs();
-    }
+        if ( cacheDir != null && !cacheDir.exists() ) {
+            if ( !cacheDir.mkdirs() ) {
+                cacheDir = null;
+            }
+        }
 
-    return cacheDir;
+        if ( cacheDir == null ) {
+            cacheDir = new File( context.getCacheDir() + File.separator + dirName );
+            cacheDir.mkdirs();
+        }
+
+        return cacheDir;
 
     }
 
