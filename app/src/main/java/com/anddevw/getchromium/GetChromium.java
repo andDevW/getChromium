96e6e0b4a731989f5eacdbe7183dc92d6567d53c
andDevW <andDevW@gmail.com>
2016-10-31 11:41:34 -0700

    Update add toast about chrome://flags

--- app/src/main/java/com/anddevw/getchromium/GetChromium.java
+++ app/src/main/java/com/anddevw/getchromium/GetChromium.java
@@ -10,6 +10,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -72,6 +73,9 @@ public class GetChromium extends AppCompatActivity {
             public void onClick(View view) {
                 // Vibrate
                 // signalInstall.vibrate(300);
+                //Enable experimental features in Chromium:
+            Snackbar.make(view, "ENABLE EXPERIMENTAL FEATURES\nIn Chromium, navigate to chrome://flags", 6000)
+                        .setAction("Action", null).show();
                 runSetup();
                 downloadLatest();
                 return;
